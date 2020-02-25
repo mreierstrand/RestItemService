d70d9d0cccdac257a48f9f051581a7b136331331
Oliver Eierstrand <oliv1115@edu.easj.dk>
2020-02-25 12:35:39 +0100

    Add project files.

---
+++ ModelLib/model/Item.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace ModelLib.model
+{
+    public class Item
+    {
+        private int _id;
+        private string _name;
+        private string _quality;
+        private double _quantity;
+
+
+        public int Id
+        {
+            get { return _id; }
+            set { Id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Quality
+        {
+            get { return _quality; }
+            set { _quality = value; }
+        }
+
+        public double Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+        public Item(int id, string name, string quality, double quantity)
+        {
+            _id = id;
+            _name = name;
+            _quality = quality;
+            _quantity = quantity;
+        }
+
+        public void Data()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} \t Name: {Name} \t Quality: {Quality} \t Quantity: {Quantity}";
+        }
+    }
+}
---
+++ RestItemService/Controllers/ItemController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib.model;
+
+namespace RestItemService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        // GET: api/Item
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return items;
+        }
+
+        // GET: api/Item/5
+        [HttpGet("{id}", Name = "Get")]
+        public Item Get(int id)
+        {
+            return items.Find(i => i.Id == id);
+        }
+
+        // POST: api/Item
+        [HttpPost]
+        public void Post([FromBody] Item value)
+        {
+            items.Add(value);
+        }
+
+        // PUT: api/Item/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Item value)
+        {
+            Item item = Get(id);
+            if (item != null)
+            {
+                item.Id = value.Id;
+                item.Name = value.Name;
+                item.Quality = value.Quality;
+                item.Quantity = value.Quantity;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Item item = Get(id);
+            items.Remove(item);
+        }
+
+        private static readonly List<Item> items = new List<Item>()
+        {
+            new Item(1, "Flaske", "High", 2),
+            new Item(2, "IPhone", "High", 1),
+            new Item(3, "Computer", "Middle", 3),
+            new Item(4,"Gibbernakker","High",100),
+            new Item(5,"Vodkashots","Low",5)
+        };
+    }
+}
